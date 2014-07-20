38d5a413832f6f20af8b2df887faa3117fc18f52
dipak1296 <dipak1296@gmail.com>
2014-07-20 07:09:02 +0530

    Pushed 2nd time consicutive by Imported to raise conflict.

--- EclipseGitProject/src/lets/test/git/GitTest.java
+++ EclipseGitProject/src/lets/test/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String args[]) {
 		System.out.println("Git is fun");
 		System.out.println("Git is Really fun");
+		System.out.println("Aha! my second consicutive push. :)");
 	}
 }
