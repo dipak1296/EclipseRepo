40de426d928847e8f384dc6700f81dbc89616b70
dipak1296 <dipak1296@gmail.com>
2014-07-20 08:27:54 +0530

    2nd type of conflict raise request

--- EclipseGitProject/src/lets/test/git/GitTest.java
+++ EclipseGitProject/src/lets/test/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
 		System.out.println("Git is Really fun");
 		System.out.println("Aha! my second consicutive push");
 		System.out.println("Working with team on GIT seems really easy.. :)");
+		System.out.println("Working on Kushwaha's Doubt : assuming this line will be discaeded!");
 	}
 }
